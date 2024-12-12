c8f883db42cd81ee5a897c1b3cc072b64031d117
Mael Pompilius <pmael@sfs-wifi-dhcp-10-143-232-106.wifi.sfsu.edu>
2024-12-11 17:48:14 -0800

    ✅~ Everything seems to work fine

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import { act } from "react";
 import App from "./App";
 
 test("renders Create Product heading", () => {
-  act(() => {
-    render(<App />);
-  });
+  render(<App />);
   const headingElement = screen.getByText(/Créer un produit/i);
   expect(headingElement).toBeInTheDocument();
 });
